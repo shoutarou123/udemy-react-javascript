0f630ef5a8d76441e4acfc8d58f5837abd878afe
shotaro <850123.a@gmail.com>
2025-03-03 14:47:21 +0900

    基礎実施中

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ColorfulMessage } from "./components/ColorfulMessage";
 
 export const App = () => {
+  console.log("-------App-------");
   const [num, setNum] = useState(0);
-  const [isShowFace, setIsShowFace] = useState(true);
+  const [isShowFace, setIsShowFace] = useState(false);
   const onClickCountUp = () => {
     setNum((prev) => prev + 1);
   };
   
+  const onClickToggle = () => {
+    setIsShowFace(!isShowFace);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        isShowFace || setIsShowFace(true);
+      } else {
+        isShowFace && setIsShowFace(false);
+      }
+    }
+  }, [num]);
+
+
+  
+  
+
   return (
     <>
       <h1 style={{ color: "red" }}>ほげ</h1>
@@ -15,7 +34,7 @@ export const App = () => {
       <ColorfulMessage color="green">元気です</ColorfulMessage> {/* colorという好きな名前でpropsを渡す blueという文字列を渡す(数字でもオブジェクトでも関数でも渡せる) */}
       <button onClick={onClickCountUp}>カウントアップボタン</button>
       <p>{num}</p>
-      <button>on/off</button>
+      <button onClick={onClickToggle}>on/off</button>
       {isShowFace && <p>!(^^)!</p>}
       
     </>
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -3,7 +3,7 @@ export const ColorfulMessage = (props) => {
   // 引数のpropsの中身は{color: 'blue', message: 'お元気ですか？'}
 
   const { color, children} = props; // prppsからcolorとchildrenを取り出すと後の記述でpropsという記述が不要になる
-
+  console.log("-------ColorfulMessage-------");
   const contentStyleA = {
     color: color, // propsの中のcolorを使用している
     fontSize: "18px"
